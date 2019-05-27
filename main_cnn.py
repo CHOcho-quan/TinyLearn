c0e2c8030e7f45a33ed7333fc82f320b7f23bb61
CHOcho-quan <574166505@qq.com>
2019-05-27 23:53:38 +0800

    Finish CNN

--- main_cnn.py
+++ main_cnn.py
@@ -4,7 +4,7 @@ import keras
 from utils import *
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 if __name__ == '__main__':
@@ -16,7 +16,10 @@ if __name__ == '__main__':
     print(y_train.shape)
 
     model = Sequential()
-    model.add(Conv2D(filters=64, kernel_size=(5, 5), activation='sigmoid', input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3])))
+    model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu', input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3])))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D())
     model.add(Flatten())
     model.add(Dense(128, activation='sigmoid'))
@@ -28,11 +31,11 @@ if __name__ == '__main__':
     model.fit(X_train, y_train, epochs=3, batch_size=32, validation_data=(X_val, y_val), callbacks=[callback])
 
     kerasPred = model.predict(X_test)
-    print(kerasPred.shape)
-    print(np.sum(kerasPred > 1), np.sum(kerasPred == 0))
     print(kerasPred)
     kerasPred = np.argmax(kerasPred, axis=1)
+    y_test = np.argmax(y_test, axis=1)
+    print(kerasPred, y_test)
     # kerasPred[kerasPred >= 0.5] = 1
     # kerasPred[kerasPred < 0.5] = 0
-    kerasAcc = np.count_nonzero(kerasPred.reshape(kerasPred.shape[0], 1) == y_test) / y_test.shape[0]
+    kerasAcc = np.count_nonzero(kerasPred == y_test) / y_test.shape[0]
     print("Keras accuracy is", kerasAcc)
